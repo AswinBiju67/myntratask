a7e4f6ec7bb7b29dd468690cda08445f97d0940f
AswinBiju67 <aswinbiju927@gmail.com>
2025-06-17 14:00:16 +0530

    update

--- lib/view/orderconfirmationscreen.dart
+++ lib/view/orderconfirmationscreen.dart
@@ -1,14 +1,43 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:lottie/lottie.dart';
+import 'package:myntratask/view/livetracking.dart';
 
-class OrderConfirmationScreen extends StatelessWidget {
+class OrderConfirmationScreen extends StatefulWidget {
    OrderConfirmationScreen({super.key});
+
+  @override
+  State<OrderConfirmationScreen> createState() => _OrderConfirmationScreenState();
+}
+
+class _OrderConfirmationScreenState extends State<OrderConfirmationScreen> {
+  
+    @override
+  void initState() {
+    super.initState();
+    // Navigate to LiveTrackingScreen after 30 seconds
+    Future.delayed(const Duration(seconds: 3), () {
+      Navigator.pushReplacement(context, MaterialPageRoute(
+        builder: (context) => Livetracking(),
+      ));
+    });
+  }
+
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-    
+      appBar: AppBar(
+        title: Text(
+          'ORDER CONFIRMATION',
+          style: GoogleFonts.roboto(
+            fontSize: 16,
+            fontWeight: FontWeight.w600,
+            color: Colors.black,
+          ),
+        ),
+  
+      ),
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
@@ -34,7 +63,7 @@ class OrderConfirmationScreen extends StatelessWidget {
              Padding(
               padding: EdgeInsets.symmetric(horizontal: 40),
               child: Text(
-                'Yay! Your order is received',
+                'Yay! Your order is received \n Your order will arrive in 30 minutes',
                 textAlign: TextAlign.center,
                 style: GoogleFonts.roboto(
                   fontSize: 16,
@@ -42,17 +71,26 @@ class OrderConfirmationScreen extends StatelessWidget {
                 ),
               ),
             ),
-            const SizedBox(height: 30),
-            // Back to home button
-            ElevatedButton(
-              onPressed: () {
-                Navigator.pop(context);
-              },
-              child: const Text('Back to Home'),
-            ),
+            SizedBox(height: 50),
+           Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+             children: [
+               Image.asset('assets/images/location logo.png', height: 50),
+               Text("Aswin, State Highway bengalu...560068",
+                 style: GoogleFonts.roboto(
+                   fontSize: 14,
+                   color: Colors.grey[700],
+                 ),
+               ),
+               SizedBox(width: 30),
+             ],
+           ),
+            SizedBox(height: 20),
+           
           ],
         ),
       ),
+      
     );
   }
 }
